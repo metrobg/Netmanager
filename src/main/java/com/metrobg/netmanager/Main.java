ecf6a31c9c18039a13e7923bc6a882fde6976d54
metrobg <egg@metrobg.com>
2020-04-26 09:55:03 -0400

    updated code to include various examples for interacting with the database

--- src/main/java/com/metrobg/netmanager/Main.java
+++ src/main/java/com/metrobg/netmanager/Main.java
@@ -1,9 +1,14 @@
 package com.metrobg.netmanager;
 
 import com.metrobg.netmanager.persistent.CustomerInfo;
+import org.apache.cayenne.Cayenne;
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.apache.cayenne.query.EJBQLQuery;
+import org.apache.cayenne.query.ObjectSelect;
 
+import java.util.List;
+/*         https://cayenne.apache.org/docs/4.0/cayenne-guide/    */
 
 public class Main {
 
@@ -12,8 +17,30 @@ public class Main {
                 .addConfig("cayenne-netmanager.xml")
                 .build();
         ObjectContext context = cayenneRuntime.newContext();
-
-        CustomerInfo customerInfo = context.newObject(CustomerInfo.class);
+        /*  select query
+        List<CustomerInfo> cust = ObjectSelect.query(CustomerInfo.class)
+                .where(CustomerInfo.CUSTOMER_NAME.likeIgnoreCase("Rader%"))
+                .select(context);
+        System.out.println("Customer address is :" + cust.get(0));
+        */
+        /*  select by primary key  */
+        CustomerInfo customerInfo = Cayenne.objectForPK(context, CustomerInfo.class, 109);
+        System.out.println("Customer Name: " + customerInfo.getCustomerName());
+ /*
+        EJBQLQuery select =
+                new EJBQLQuery("select a FROM Artist a WHERE a.name =EJBQLQuery select =
+        new EJBQLQuery("select a FROM Artist a WHERE a.name = 'Salvador Dali'");
+        List<Artist> artists = context.performQuery(select);
+        EJBQLQuery delete = new EJBQLQuery("delete from Painting");
+        context.performGenericQuery(delete);
+        EJBQLQuery update =
+                new EJBQLQuery("UPDATE Painting AS p SET p.name = 'P2' WHERE p.name = 'P1'");
+        context.performGenericQuery(update);
+        'Salvador Dali' ");
+        List<Artist> artists = context.performQuery(select);
+         */
+       /*    record insert
+       CustomerInfo customerInfo = context.newObject(CustomerInfo.class);
         customerInfo.setCustomerName("Rader Foods");
         customerInfo.setCustomerAddress1("16390 NW 52nd Ave");
         customerInfo.setCustomerCity("Miami");
@@ -21,5 +48,6 @@ public class Main {
         customerInfo.setCustomerZip("33014");
         customerInfo.setCustomerPhone("3056282217");
         context.commitChanges();
+        */
     }
 }
